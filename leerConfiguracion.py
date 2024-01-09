8f0177a46b1a571d13d0a6c86999f5829570e539
BraulioPerdomo <150398810+BraulioPerdomo@users.noreply.github.com>
2024-01-09 17:24:38 -0600

    .

--- leerConfiguracion.py
+++ leerConfiguracion.py
@@ -1,14 +1,20 @@
 velocidadDeParpadeo = 1
 
-f = open("configuracion.ini", "r")
-lineas = f.readlines()
+def leerConfiguracion():
+    
+    f = open("configuracion.ini", "r")
+    lineas = f.readlines()
 
-#print(lineas)
-for linea in lineas:
-    datos = linea.split('=')
-    if (datos[0] == "velocidad"):
-        velocidadDeParpadeo = int(datos[1])
-    #print(datos)
-f.close()
+    #print(lineas)
+    for linea in lineas:
+        datos = linea.split('=')
+        if (datos[0] == "velocidad"):
+            velocidadDeParpadeo = int(datos[1])
+        #print(datos)
+    f.close()
 
 #print(f"Valocidad: {velocidadDeParpadeo}")
+
+leerConfiguracion()
+
+print (f"velocidad: {velocidadDeParpadeo}")
