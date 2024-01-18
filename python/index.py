4d7f1d9ded1b5114cdc6a035439dec2f04b0a66b
BraulioPerdomo <perdomobraulio15@gmail.com>
2024-01-17 19:16:13 -0600

    .

--- python/index.py
+++ python/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola"
+    return "Hola que tal como estas?"
 
 if __name__ == '__main__':
     app.run (debug=True, port=80, host='0.0.0.0')
